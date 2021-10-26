a2b17e77477a9bf37116b12293e192386a5f5ccd
Ducky3264 <david.love.wii@gmail.com>
2021-10-26 00:52:24 -0500

    Initial commit

---
+++ Controllers/CurrentStateController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Arduinoapi.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class CurrentStateController : ControllerBase
+    {
+        public async Task<bool> GetState()
+        {
+            
+            string text = System.IO.File.ReadAllText(Directory.GetCurrentDirectory() + "\\state.txt");
+            if (text == "on")
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+        public async Task ChangeState()
+        {
+            if (await GetState())
+            {
+                await System.IO.File.WriteAllTextAsync(Directory.GetCurrentDirectory() + "\\state.txt", "off");
+            } else
+            {
+                await System.IO.File.WriteAllTextAsync(Directory.GetCurrentDirectory() + "\\state.txt", "on");
+            }
+        }
+        private readonly ILogger<CurrentStateController> _logger;
+        public CurrentStateController(ILogger<CurrentStateController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("getState")]
+        public async Task<IActionResult> Get()
+        {
+            
+            return Ok(await GetState() ? "On" : "Off");
+        }
+        [HttpGet]
+        [Route("changeState")]
+        public async Task<IActionResult> Change()
+        {
+            ChangeState();
+            return Ok("State Changed");
+            
+
+        }
+    }
+}
